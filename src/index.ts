68f007c9f40a64a632251e2fc9bc25173ad3da2a
Denys <kukildenys@gmail.com>
2024-01-29 20:02:49 +0100

    mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,39 @@
 import express from "express"
-import compression from "compression";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import http from "http";
 dotenv.config();
 
 const PORT = process.env.SERVER_PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
 
 
 const app = express();
 app.use(express.json());
-app.use(compression())
+
 
 const server = http.createServer(app)
 
-
-const start = async () => {
-
-    server.listen(3000, () => {
-        console.log(` app listening on port 3000`);
-    })
-    
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(MONGO_URL)
+        console.log("MongoDB connected!");
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1)
+    }
 }
+
+
+const start = () => {
+    try {
+      server.listen(PORT, () => {
+        console.log(`Server has been started on port: ${PORT}`);
+      });
+      connectDB();
+    } catch (error) {
+      console.error(`Error starting server: ${error.message}`);
+    }
+  };
 
 start()
